0d32ecc7acd45d2e5d2ab5f837e6564154cb9fe3
Emma Finkel <emma.finkel@gmail.com>
2023-08-10 11:31:36 -0400

    Initial Commit

---
+++ Navigation Wave 5 Test/Navigation_Wave_5_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_Wave_5_TestApp.swift
+//  Navigation Wave 5 Test
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_Wave_5_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
